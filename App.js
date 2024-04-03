b07bc2eca1ce37c953ea1f878e670d0fa628f40e
nileshsingh169 <118014052+nileshsingh169@users.noreply.github.com>
2024-04-03 23:46:32 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,68 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+import ImageSearchForm from "./components/ImageSearchForm/ImageSearchForm";
+import FaceDetect from "./components/FaceDetect/FaceDetect";
+import "./App.css";
 
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+// You need to add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: "a362798b697b4432b2811f2d44c7aaad"
 
-import Navigation from './components/Navigation/Navigation';
-import SignInPage from './pages/SignInPage/SignInPage';
-import HomePage from './pages/HomePage/HomePage';
-import AppPage from './pages/AppPage/AppPage';
-import Credits from './components/Credits/Credits';
+});
 
-import { loadUser } from './redux/auth/authActions';
-import { store } from './redux/store';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+    };
+  }
 
-import './App.scss';
-
-class App extends React.Component {
-  componentDidMount = () => {
-    store.dispatch(loadUser());
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
   };
 
-  render = () => (
-    <div className='app'>
-      <Navigation />
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
 
-      <main id='main'>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/sign_in' component={SignInPage} />
-          <ProtectedRoute exact path='/face_detect' component={AppPage} />
-        </Switch>
-      </main>
-      <Credits />
-    </div>
-  );
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ImageSearchForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
+        <FaceDetect box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
